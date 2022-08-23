86ace1f78c9db2800a20925ed857a50f27aa8e79
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-23 20:44:38 +0500

    setup main

--- app/src/main/java/xyz/teamgravity/kichkinashahzoda/presentation/activity/Main.kt
+++ app/src/main/java/xyz/teamgravity/kichkinashahzoda/presentation/activity/Main.kt
@@ -7,15 +7,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import dagger.hilt.android.AndroidEntryPoint
+import xyz.teamgravity.kichkinashahzoda.presentation.navigation.Navigation
 import xyz.teamgravity.kichkinashahzoda.presentation.theme.KichkinaShahzodaTheme
 
+@AndroidEntryPoint
 class Main : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             KichkinaShahzodaTheme {
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Navigation()
                 }
             }
         }
