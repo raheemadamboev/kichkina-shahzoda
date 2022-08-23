4e73cbd7716c0606a426b204d0b60fadf8d79fa9
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-23 20:49:28 +0500

    add support portrait screen

---
+++ app/src/main/java/xyz/teamgravity/kichkinashahzoda/presentation/screen/support/SupportPortraitScreen.kt
@@ -0,0 +1,10 @@
+package xyz.teamgravity.kichkinashahzoda.presentation.screen.support
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SupportPortraitScreen(
+    onBackButtonClick: () -> Unit,
+) {
+
+}
