5aae15c382a51394f17fbd740d10e3595af4471e
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-23 20:49:13 +0500

    build support screen

--- app/src/main/java/xyz/teamgravity/kichkinashahzoda/presentation/screen/support/SupportScreen.kt
+++ app/src/main/java/xyz/teamgravity/kichkinashahzoda/presentation/screen/support/SupportScreen.kt
@@ -1,8 +1,20 @@
 package xyz.teamgravity.kichkinashahzoda.presentation.screen.support
 
 import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import xyz.teamgravity.kichkinashahzoda.presentation.component.misc.WindowInfo
+import xyz.teamgravity.kichkinashahzoda.presentation.component.misc.rememberWindowInfo
+import xyz.teamgravity.kichkinashahzoda.presentation.navigation.MainNavGraph
 
+@MainNavGraph
+@Destination
 @Composable
-fun SupportScreen() {
-
+fun SupportScreen(
+    navigator: DestinationsNavigator,
+) {
+    when (rememberWindowInfo().screenWidthInfo) {
+        WindowInfo.WindowType.Compact -> SupportPortraitScreen(onBackButtonClick = navigator::popBackStack)
+        else -> SupportLandscapeScreen(onBackButtonClick = navigator::popBackStack)
+    }
 }
