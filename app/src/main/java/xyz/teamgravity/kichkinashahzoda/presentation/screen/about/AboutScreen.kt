f7e349d5a149226eaa23f030cd37e9bbdebd3e03
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-23 20:47:36 +0500

    add about screen

--- app/src/main/java/xyz/teamgravity/kichkinashahzoda/presentation/screen/about/AboutScreen.kt
+++ app/src/main/java/xyz/teamgravity/kichkinashahzoda/presentation/screen/about/AboutScreen.kt
@@ -1,8 +1,20 @@
 package xyz.teamgravity.kichkinashahzoda.presentation.screen.about
 
 import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import xyz.teamgravity.kichkinashahzoda.presentation.component.misc.WindowInfo
+import xyz.teamgravity.kichkinashahzoda.presentation.component.misc.rememberWindowInfo
+import xyz.teamgravity.kichkinashahzoda.presentation.navigation.MainNavGraph
 
+@MainNavGraph
+@Destination
 @Composable
-fun AboutScreen() {
-
+fun AboutScreen(
+    navigator: DestinationsNavigator,
+) {
+    when (rememberWindowInfo().screenWidthInfo) {
+        WindowInfo.WindowType.Compact -> AboutPortraitScreen(onBackButtonClick = navigator::popBackStack)
+        else -> AboutLandscapeScreen(onBackButtonClick = navigator::popBackStack)
+    }
 }
