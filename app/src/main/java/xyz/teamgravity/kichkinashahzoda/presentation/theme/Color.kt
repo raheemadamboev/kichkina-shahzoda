b043f6c7a5d2cb7123e2341d509db954c74492ad
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-23 18:45:10 +0500

    setup material3 colors

---
+++ app/src/main/java/xyz/teamgravity/kichkinashahzoda/presentation/theme/Color.kt
@@ -0,0 +1,43 @@
+package xyz.teamgravity.kichkinashahzoda.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Pink10 = Color(0xFF330000)
+val Pink20 = Color(0xFF660000)
+val Pink30 = Color(0xFF990000)
+val Pink40 = Color(0xFFFFADAD)
+val Pink80 = Color(0xFFFF9999)
+val Pink90 = Color(0xFFFFCCCC)
+
+val DeepPink10 = Color(0xFF250E10)
+val DeepPink20 = Color(0xFF4A1C20)
+val DeepPink30 = Color(0xFF6F2A30)
+val DeepPink40 = Color(0xFFCA7D7E)
+val DeepPink80 = Color(0xFFE3B5B9)
+val DeepPink90 = Color(0xFFF1DADC)
+
+val Violet10 = Color(0xFF330033)
+val Violet20 = Color(0xFF660066)
+val Violet30 = Color(0xFF990099)
+val Violet40 = Color(0xFFCC00CC)
+val Violet80 = Color(0xFFFF99FF)
+val Violet90 = Color(0xFFFFCCFF)
+
+val Red10 = Color(0xFF410001)
+val Red20 = Color(0xFF680003)
+val Red30 = Color(0xFF930006)
+val Red40 = Color(0xFFBA1B1B)
+val Red80 = Color(0xFFFFB4A9)
+val Red90 = Color(0xFFFFDAD4)
+
+val Grey10 = Color(0xFF191C1D)
+val Grey20 = Color(0xFF2D3132)
+val Grey90 = Color(0xFFE0E3E3)
+val Grey95 = Color(0xFFEFF1F1)
+val Grey99 = Color(0xFFFBFDFD)
+
+val PinkGrey30 = Color(0xFF594041)
+val PinkGrey50 = Color(0xFF946B6C)
+val PinkGrey60 = Color(0xFFA98989)
+val PinkGrey80 = Color(0xFFD4C4C4)
+val PinkGrey90 = Color(0xFFEAE1E1)
