256c0a500f6c89c46241779eaaec98e90ae08e48
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-23 20:48:21 +0500

    add song screen

--- app/src/main/java/xyz/teamgravity/kichkinashahzoda/presentation/screen/song/SongScreen.kt
+++ app/src/main/java/xyz/teamgravity/kichkinashahzoda/presentation/screen/song/SongScreen.kt
@@ -1,8 +1,22 @@
 package xyz.teamgravity.kichkinashahzoda.presentation.screen.song
 
 import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import xyz.teamgravity.kichkinashahzoda.presentation.component.misc.WindowInfo
+import xyz.teamgravity.kichkinashahzoda.presentation.component.misc.rememberWindowInfo
+import xyz.teamgravity.kichkinashahzoda.presentation.navigation.MainNavGraph
+import xyz.teamgravity.kichkinashahzoda.presentation.screen.support.SupportLandscapeScreen
+import xyz.teamgravity.kichkinashahzoda.presentation.screen.support.SupportPortraitScreen
 
+@MainNavGraph
+@Destination
 @Composable
-fun SongScreen() {
-
+fun SongScreen(
+    navigator: DestinationsNavigator,
+) {
+    when (rememberWindowInfo().screenWidthInfo) {
+        WindowInfo.WindowType.Compact -> SupportPortraitScreen(onBackButtonClick = navigator::popBackStack)
+        else -> SupportLandscapeScreen(onBackButtonClick = navigator::popBackStack)
+    }
 }
