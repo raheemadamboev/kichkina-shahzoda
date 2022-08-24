1b4c1dca062c7d978cc942752167d09881f16587
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-24 14:17:20 +0500

    add song screen nav args
    Took 30 seconds

--- app/src/main/java/xyz/teamgravity/kichkinashahzoda/presentation/screen/song/SongScreen.kt
+++ app/src/main/java/xyz/teamgravity/kichkinashahzoda/presentation/screen/song/SongScreen.kt
@@ -6,17 +6,19 @@ import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import xyz.teamgravity.kichkinashahzoda.presentation.component.misc.WindowInfo
 import xyz.teamgravity.kichkinashahzoda.presentation.component.misc.rememberWindowInfo
 import xyz.teamgravity.kichkinashahzoda.presentation.navigation.MainNavGraph
-import xyz.teamgravity.kichkinashahzoda.presentation.screen.support.SupportLandscapeScreen
-import xyz.teamgravity.kichkinashahzoda.presentation.screen.support.SupportPortraitScreen
 
 @MainNavGraph
-@Destination
+@Destination(navArgsDelegate = SongScreenNavArgs::class)
 @Composable
 fun SongScreen(
     navigator: DestinationsNavigator,
 ) {
     when (rememberWindowInfo().screenWidthInfo) {
-        WindowInfo.WindowType.Compact -> SupportPortraitScreen(onBackButtonClick = navigator::popBackStack)
-        else -> SupportLandscapeScreen(onBackButtonClick = navigator::popBackStack)
+        WindowInfo.WindowType.Compact -> SongPortraitScreen(onBackButtonClick = navigator::popBackStack)
+        else -> SongLandscapeScreen(onBackButtonClick = navigator::popBackStack)
     }
 }
+
+data class SongScreenNavArgs(
+    val id: Int,
+)
