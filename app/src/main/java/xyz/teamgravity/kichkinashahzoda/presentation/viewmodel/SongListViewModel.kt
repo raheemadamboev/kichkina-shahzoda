7b1b984fff80eed6d3f7a0ccba3fd511d1fde5c6
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-24 14:25:08 +0500

    setup song list viewmodel
    Took 4 minutes

--- app/src/main/java/xyz/teamgravity/kichkinashahzoda/presentation/viewmodel/SongListViewModel.kt
+++ app/src/main/java/xyz/teamgravity/kichkinashahzoda/presentation/viewmodel/SongListViewModel.kt
@@ -5,12 +5,19 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import xyz.teamgravity.kichkinashahzoda.data.model.SongModel
+import xyz.teamgravity.kichkinashahzoda.data.repository.MainRepository
 import javax.inject.Inject
 
 @HiltViewModel
 class SongListViewModel @Inject constructor(
-
+    repository: MainRepository,
 ) : ViewModel() {
+
+    val songs: List<SongModel> by mutableStateOf(repository.getSongs())
+
+    var currentSong: SongModel  by mutableStateOf(songs.first())
+        private set
 
     var menuExpanded: Boolean by mutableStateOf(false)
         private set
