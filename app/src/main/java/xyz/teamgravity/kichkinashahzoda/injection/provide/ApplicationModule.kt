5968211d136131d456a04aa9ce140c7a7de67230
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-26 21:20:15 +0500

    provide singleton dependencies
    Took 28 minutes

--- app/src/main/java/xyz/teamgravity/kichkinashahzoda/injection/provide/ApplicationModule.kt
+++ app/src/main/java/xyz/teamgravity/kichkinashahzoda/injection/provide/ApplicationModule.kt
@@ -1,9 +1,13 @@
 package xyz.teamgravity.kichkinashahzoda.injection.provide
 
+import android.app.Application
+import com.google.android.exoplayer2.upstream.DefaultDataSource
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import timber.log.Timber
+import xyz.teamgravity.kichkinashahzoda.core.service.SongServiceConnection
 import xyz.teamgravity.kichkinashahzoda.data.repository.MainRepository
 import javax.inject.Singleton
 
@@ -13,5 +17,18 @@ object ApplicationModule {
 
     @Provides
     @Singleton
-    fun provideMainRepository(): MainRepository = MainRepository()
+    fun provideDefaultDataSourceFactory(application: Application): DefaultDataSource.Factory = DefaultDataSource.Factory(application)
+
+    @Provides
+    @Singleton
+    fun provideMainRepository(defaultDataSourceFactory: DefaultDataSource.Factory): MainRepository =
+        MainRepository(defaultDataSourceFactory)
+
+    @Provides
+    @Singleton
+    fun provideSongServiceConnection(application: Application): SongServiceConnection = SongServiceConnection(application)
+
+    @Provides
+    @Singleton
+    fun provideTimberDebugTree(): Timber.DebugTree = Timber.DebugTree()
 }
