4b66b105c95d88140c21d3e0094032a938d20404
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-26 21:28:29 +0500

    add song model
    Took 18 seconds

--- app/src/main/java/xyz/teamgravity/kichkinashahzoda/data/model/SongModel.kt
+++ app/src/main/java/xyz/teamgravity/kichkinashahzoda/data/model/SongModel.kt
@@ -1,10 +1,6 @@
 package xyz.teamgravity.kichkinashahzoda.data.model
 
-import androidx.annotation.RawRes
-import androidx.annotation.StringRes
-
 data class SongModel(
-    val id: Int,
-    @StringRes val name: Int,
-    @RawRes val song: Int,
+    val id: String,
+    val name: String,
 )
