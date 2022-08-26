ab9da423cca91481e635b28672a246d5afd72857
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-26 21:27:37 +0500

    add service module
    Took 54 seconds

---
+++ app/src/main/java/xyz/teamgravity/kichkinashahzoda/injection/provide/ServiceModule.kt
@@ -0,0 +1,33 @@
+package xyz.teamgravity.kichkinashahzoda.injection.provide
+
+import android.app.Application
+import com.google.android.exoplayer2.C
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.audio.AudioAttributes
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ServiceComponent
+import dagger.hilt.android.scopes.ServiceScoped
+
+@Module
+@InstallIn(ServiceComponent::class)
+object ServiceModule {
+
+    @Provides
+    @ServiceScoped
+    fun provideAudioAttributes(): AudioAttributes = AudioAttributes.Builder()
+        .setContentType(C.AUDIO_CONTENT_TYPE_MUSIC)
+        .setUsage(C.USAGE_MEDIA)
+        .build()
+
+    @Provides
+    @ServiceScoped
+    fun provideExoPlayer(application: Application, audioAttributes: AudioAttributes): ExoPlayer =
+        ExoPlayer.Builder(application).build().apply {
+            setAudioAttributes(audioAttributes, true)
+            setHandleAudioBecomingNoisy(true)
+        }
+}
