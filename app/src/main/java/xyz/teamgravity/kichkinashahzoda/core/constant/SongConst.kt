ba15205c81b2a74745cb34cad2d74da441f45f45
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-26 21:27:54 +0500

    add constants for song
    Took 19 seconds

---
+++ app/src/main/java/xyz/teamgravity/kichkinashahzoda/core/constant/SongConst.kt
@@ -0,0 +1,10 @@
+package xyz.teamgravity.kichkinashahzoda.core.constant
+
+object SongConst {
+
+    const val NAME_1 = "Birinchi qism"
+    const val NAME_2 = "Ikkinchi qism"
+    const val NAME_3 = "Yakuniy qism"
+
+    const val ARTIST = "Antoine de Saint-Exupéry"
+}
