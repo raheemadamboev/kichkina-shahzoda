963870b76e3984844e20ed99c548b12c6a1a29d7
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-26 21:26:41 +0500

    add media metadata compat mapper
    Took 3 minutes

---
+++ app/src/main/java/xyz/teamgravity/kichkinashahzoda/data/mapper/MediaMetadataCompatMapper.kt
@@ -0,0 +1,25 @@
+package xyz.teamgravity.kichkinashahzoda.data.mapper
+
+import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaDescriptionCompat
+import android.support.v4.media.MediaMetadataCompat
+import androidx.core.net.toUri
+import xyz.teamgravity.kichkinashahzoda.data.model.SongModel
+
+fun MediaMetadataCompat.toMediaItem(): MediaBrowserCompat.MediaItem {
+    val description = MediaDescriptionCompat.Builder()
+        .setMediaUri(getString(MediaMetadataCompat.METADATA_KEY_MEDIA_URI).toUri())
+        .setTitle(description.title)
+        .setSubtitle(description.subtitle)
+        .setMediaId(description.mediaId)
+        .setIconUri(description.iconUri)
+        .build()
+    return MediaBrowserCompat.MediaItem(description, MediaBrowserCompat.MediaItem.FLAG_PLAYABLE)
+}
+
+fun MediaMetadataCompat.toSong(): SongModel {
+    return SongModel(
+        id = description.mediaId!!,
+        name = description.title.toString()
+    )
+}
