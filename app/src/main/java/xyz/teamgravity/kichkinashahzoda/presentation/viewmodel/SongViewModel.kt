b26406d429be437412d436f09e756c62fc63d6c5
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-27 14:44:41 +0500

    add song viewmodel
    Took 13 seconds

---
+++ app/src/main/java/xyz/teamgravity/kichkinashahzoda/presentation/viewmodel/SongViewModel.kt
@@ -0,0 +1,14 @@
+package xyz.teamgravity.kichkinashahzoda.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SongViewModel @Inject constructor(
+
+) : ViewModel() {
+
+
+
+}
