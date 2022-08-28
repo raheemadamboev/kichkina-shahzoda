7b0dc38f7efd1d9ea31402a9ddb7630c4695cdf0
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-28 19:05:43 +0500

    final refactoring
    Took 11 minutes

--- app/src/main/java/xyz/teamgravity/kichkinashahzoda/core/extension/PlaybackStateCompatExtension.kt
+++ app/src/main/java/xyz/teamgravity/kichkinashahzoda/core/extension/PlaybackStateCompatExtension.kt
@@ -3,18 +3,9 @@ package xyz.teamgravity.kichkinashahzoda.core.extension
 import android.os.SystemClock
 import android.support.v4.media.session.PlaybackStateCompat
 
-inline val PlaybackStateCompat.isPrepared: Boolean
-    get() = state == PlaybackStateCompat.STATE_BUFFERING ||
-            state == PlaybackStateCompat.STATE_PLAYING ||
-            state == PlaybackStateCompat.STATE_PAUSED
-
 inline val PlaybackStateCompat.isPlaying: Boolean
     get() = state == PlaybackStateCompat.STATE_BUFFERING ||
             state == PlaybackStateCompat.STATE_PLAYING
-
-inline val PlaybackStateCompat.isPlayEnabled: Boolean
-    get() = actions and PlaybackStateCompat.ACTION_PLAY != 0L ||
-            (actions and PlaybackStateCompat.ACTION_PLAY_PAUSE != 0L && state == PlaybackStateCompat.STATE_PAUSED)
 
 inline val PlaybackStateCompat.currentPlaybackPosition: Long
     get() = if (state == PlaybackStateCompat.STATE_PLAYING) {
